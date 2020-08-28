a0689256f261e802f5a18e240336bcc591782e55
Deaniesaur <dean.pinlac@gmail.com>
2020-08-29 00:56:52 +0800

    Added Title

---
+++ src/components/title.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { TextField } from '@material-ui/core';
+import { render } from '@testing-library/react';
+
+export const title = <div style={{color:"#ffffff"}}>Dean Pinlac</div>
