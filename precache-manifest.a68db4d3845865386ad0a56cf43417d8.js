ee9d965e16dbd599db40353c59356817b388f6e1
Deaniesaur <dean.pinlac@gmail.com>
2020-08-28 23:10:44 +0800

    Updates

--- precache-manifest.a68db4d3845865386ad0a56cf43417d8.js
+++ precache-manifest.a68db4d3845865386ad0a56cf43417d8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9a8a516f296ac83984a040750d9e222e",
+    "revision": "fb63b58659274da63a8f0540b64472fa",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "f7085faccf63ce36551f",
-    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+    "revision": "e80700d74aa3e2609d7d",
+    "url": "/portfolio/static/css/main.523f09c5.chunk.css"
   },
   {
     "revision": "6e8153c06218235d1b09",
@@ -16,15 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.b02c08dd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f7085faccf63ce36551f",
-    "url": "/portfolio/static/js/main.681375d0.chunk.js"
+    "revision": "e80700d74aa3e2609d7d",
+    "url": "/portfolio/static/js/main.ac39cc8b.chunk.js"
   },
   {
     "revision": "80bb2f60db2a241fd581",
     "url": "/portfolio/static/js/runtime-main.c1ffeb8c.js"
   },
   {
-    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2801fca27922877a754297ef7b9e6b95.js"
+  "/portfolio/precache-manifest.a68db4d3845865386ad0a56cf43417d8.js"
 );
 
 self.addEventListener('message', (event) => {
