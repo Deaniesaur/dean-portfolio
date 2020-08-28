0c2282c8a63fc0a9e42dbf5552312a5713f212a6
Deaniesaur <dean.pinlac@gmail.com>
2020-08-28 22:36:38 +0800

    Updates

---
+++ precache-manifest.2801fca27922877a754297ef7b9e6b95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8a516f296ac83984a040750d9e222e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f7085faccf63ce36551f",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e8153c06218235d1b09",
+    "url": "/portfolio/static/js/2.b02c08dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b02c08dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7085faccf63ce36551f",
+    "url": "/portfolio/static/js/main.681375d0.chunk.js"
+  },
+  {
+    "revision": "80bb2f60db2a241fd581",
+    "url": "/portfolio/static/js/runtime-main.c1ffeb8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
